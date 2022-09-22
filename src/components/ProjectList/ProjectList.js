ac0488f3cbf21f3226770cac209ac0b17f477f92
ntidke22 <107246716+ntidke22@users.noreply.github.com>
2022-09-22 17:10:49 +0530

    Update ProjectList.js

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -6,7 +6,7 @@ const ProjectList = (props) => {
   const linkMarkup = props.projoptions.map((link) => (
     <li key={link.id} className="link-list-item"  >
       <div id="inner">
-       <b>{link.title}</b><br/><hr/>{link.text}<br/><b>Technical Stack:</b>{link.stack}<br/><b>Host Link:</b><a href={link.host} rel="noreferrer" target={"_blank"}>{link.title}</a><br/>{"\u00a0\u00a0"}
+       <b>{link.title}</b><br/><hr/>{link.text}<br/><b>Technical Stack:</b>{link.stack}<br/><b>Host Link:</b><a href={link.host} rel={"noreferrer"} target={"_blank"}>{link.title}</a><br/>{"\u00a0\u00a0"}
        </div>{"\u00a0"}{"\n"}{"\n"}
     </li>
   
